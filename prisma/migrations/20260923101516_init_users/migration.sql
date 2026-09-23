a756ba3afad7886f0d8a34b7bbac894148e115c4
MOHAMMAD ARFAT <mdarfatyunus@gmail.com>
2026-09-23 19:49:51 +0600

    feat(db): add User model and initial migration

---
+++ prisma/migrations/20260923101516_init_users/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('PASSENGER', 'DRIVER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
