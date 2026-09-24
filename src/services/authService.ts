12d976e27314e9accc2fd8625fc95364d387b176
Mohammad Arfat <140711122+yunusarfat@users.noreply.github.com>
2026-09-24 23:43:13 +0600

    Update authService.ts

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import prisma from "../db/prismaClient.js";
+import prisma from "../db/prismaclient.js";
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
 const SALT_ROUNDS = 10;
